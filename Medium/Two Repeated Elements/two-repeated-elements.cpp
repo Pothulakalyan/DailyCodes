623da4ef81c0d61fba819b5dde2f8e8576f26ebb
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2024-04-18 20:56:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Two Repeated Elements/two-repeated-elements.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int n) {
+        // Your code here
+        vector<int>ve;
+        map<int,int>mpp;
+        for(int i=0;i<n+2;i++){
+            mpp[arr[i]]++;
+            if(mpp[arr[i]]==2){
+                ve.push_back(arr[i]);
+            }
+        }
+        return ve;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
