922c6d03e80fcd11f3978173d44f3fe3f6fc868f
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2024-07-01 11:40:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make Binary Tree From Linked List/make-binary-tree-from-linked-list.cpp
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void push(struct Node **head_ref, int new_data) {
+
+    struct Node *new_node = new Node(new_data);
+
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void convert(Node *head, TreeNode *&root);
+
+void lvl(TreeNode *r) {
+    if (r == NULL)
+        return;
+    queue<TreeNode *> q;
+    q.push(r);
+    while (!q.empty()) {
+        TreeNode *j = q.front();
+        cout << j->data << " ";
+        q.pop();
+        if (j->left)
+            q.push(j->left);
+        if (j->right)
+            q.push(j->right);
+    }
+}
+
+void reverses(Node **head) {
+    Node *prev = NULL;
+    Node *cur = *head;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *head = prev;
+}
+
+/* Driver program to test above function*/
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL;
+
+        cin >> n;
+        for (i = 1; i <= n; i++) {
+            cin >> l;
+            push(&head, l);
+        }
+        reverses(&head);
+        Node *prevhead = head;
+        TreeNode *root = NULL;
+        convert(head, root);
+        if (root == NULL) {
+            cout << "-1\n";
+        } else if (root == NULL && prevhead != NULL) {
+            cout << "-1\n";
+        } else {
+            lvl(root);
+            cout << endl;
+        }
+        // inorder(root);
+        getchar();
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+The structure of Link list Node is as follows
+struct Node
+{
+    int data;
+    struct node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+The structure of TreeNode is as follows
+struct TreeNode
+{
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+};
+*/
+
+// Function to make binary tree from linked list.
+void creating_BT(int ind,vector<int>&ve,TreeNode* &root){
+    if(2*ind+1<ve.size()){
+        TreeNode *nn= new TreeNode(ve[2*ind+1]);
+        root->left=nn;
+        creating_BT(2*ind+1,ve,root->left);
+    }
+    if(2*ind+2<ve.size()){
+        TreeNode *nn=new TreeNode(ve[2*ind+2]);
+        root->right=nn;
+        creating_BT(2*ind+2,ve,root->right);
+    }
+    
+}
+void convert(Node *head, TreeNode *&root) {
+    // Your code here
+    vector<int>ve;
+    while(head!=NULL){
+        ve.push_back(head->data);
+        head=head->next;
+    }
+    TreeNode* temp=new TreeNode(ve[0]);
+    root=temp;
+    creating_BT(0,ve,root);
+    
+}
