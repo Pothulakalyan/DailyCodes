497c01a77031657a50e0103562ef8a6e06f6008e
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2024-11-20 09:25:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.cpp
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -10,38 +10,61 @@ class Solution {
   public:
     void nextPermutation(vector<int>& arr) {
         // code here
-        int n = arr.size();
-        int ans = -1;
+    //     int n = arr.size();
+    //     int ans = -1;
 
-    // Step 1: Find the first decreasing element from the right
-        for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] < arr[i + 1]) {
-                ans = i;
-                break;
-            }
-        }
+    // // Step 1: Find the first decreasing element from the right
+    //     for (int i = n - 2; i >= 0; i--) {
+    //         if (arr[i] < arr[i + 1]) {
+    //             ans = i;
+    //             break;
+    //         }
+    //     }
 
-    // Step 2: If no such element is found, reverse the array
-        if (ans == -1) {
-            reverse(arr.begin(), arr.end());
-        } 
-        else {
-        // Step 3: Find the smallest element greater than arr[ans] from the right
-            int ind = -1;
-            for (int i = n - 1; i > ans; i--) {
-                if (arr[ans] < arr[i]) {
-                    ind = i;
-                    break;
+    // // Step 2: If no such element is found, reverse the array
+    //     if (ans == -1) {
+    //         reverse(arr.begin(), arr.end());
+    //     } 
+    //     else {
+    //     // Step 3: Find the smallest element greater than arr[ans] from the right
+    //         int ind = -1;
+    //         for (int i = n - 1; i > ans; i--) {
+    //             if (arr[ans] < arr[i]) {
+    //                 ind = i;
+    //                 break;
+    //             }
+    //         }
+
+    //     // Step 4: Swap the elements
+    //         swap(arr[ans], arr[ind]);
+
+    //     // Step 5: Reverse the elements to the right of ans
+    //         reverse(arr.begin() + ans + 1, arr.end());
+    //     }
+           int si=arr.size();
+           int ans=-1,ind=-1;
+           for(int i=si-2;i>=0;i--){
+               if(arr[i]<arr[i+1]){
+                   ans=i;
+                   break;
+               }
+           }
+           if(ans!=-1){
+                for(int i=si-1;i>ans;i--){
+                    if(arr[i]>arr[ans]){
+                        ind=i;
+                        break;
+                   }
                 }
-            }
-
-        // Step 4: Swap the elements
-            swap(arr[ans], arr[ind]);
-
-        // Step 5: Reverse the elements to the right of ans
-            reverse(arr.begin() + ans + 1, arr.end());
-        }
-        
+                swap(arr[ans],arr[ind]);
+                reverse(arr.begin()+ans+1,arr.end());
+           }else{
+                reverse(arr.begin(),arr.end());
+           
+           }
+           
+           
+    
 
         
         
