d3f334a444e5d14faba76c10367f10f38c3cccd4
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2025-01-14 07:54:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -8,47 +8,20 @@ using namespace std;
 class Solution {
   public:
     // Function to find equilibrium point in the array.
-    // arr: input array
-    int equilibriumPoint(vector<long long> &a) {
-        // Your code here
-        //int l=0,r=0,res=0;
-        // for(int i=0;i<arr.size();i++){
-        //     r+=arr[i];
-        // }
-        // for(int i=0;i<arr.size();i++){
-        //     r-=arr[i];
-        //     if(l==r){
-        //         res=i;
-        //     }
-        //     else{
-        //         l+=arr[i];    
-        //     }
-            
-        // }
-        // if(res){
-        //     return res+1;
-        // }
-        // return {-1};
-        int n=a.size();
-        int left_sum=0,right_sum=0;
-        for(int i=0;i<n;i++)
-        {
-            right_sum+=a[i];
+    int findEquilibrium(vector<int> &arr) {
+        // code here
+        int r_su=0,l_su=0;
+        for(int i=0;i<arr.size();i++){
+            r_su+=arr[i];
         }
-        for(int i=0;i<n;i++)
-        {
-            right_sum-=a[i];
-            if(left_sum==right_sum)
-            {
-                return i+1;
-                break;
+        for(int i=0;i<arr.size();i++){
+            r_su-=arr[i];
+            if(l_su==r_su){
+                return i;
             }
-            else
-            {
-                left_sum+=a[i];
-            }
+            l_su+=arr[i];
         }
-        return {-1};
+        return -1;
     }
 };
 
@@ -60,7 +33,7 @@ int main() {
     cin.ignore(); // To discard any leftover newline characters
     while (t--)   // while testcases exist
     {
-        vector<long long> arr;
+        vector<int> arr;
         string input;
         getline(cin, input); // Read the entire line for the array elements
         stringstream ss(input);
@@ -70,7 +43,8 @@ int main() {
         }
 
         Solution ob;
-        cout << ob.equilibriumPoint(arr) << endl;
+        cout << ob.findEquilibrium(arr) << endl;
+        cout << "~" << endl;
     }
 }
 // } Driver Code Ends
