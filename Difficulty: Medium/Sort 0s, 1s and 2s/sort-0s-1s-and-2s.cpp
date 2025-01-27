9237a1668ce883904cb79ed52b680f7238858508
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2025-01-27 05:50:46 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        
+        //using two pointers by GFG Practice youtube channel
+        int n=arr.size();
+        int l=0;
+        int i=0;
+        int r=n-1;
+        while(i<=r){
+            if(arr[i]==1){
+                i++;
+            }
+            else if(arr[i]==2){
+                swap(arr[i],arr[r]);
+                r--;
+            }
+            else{
+                swap(arr[i],arr[l]);
+                l++;
+                i++;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
