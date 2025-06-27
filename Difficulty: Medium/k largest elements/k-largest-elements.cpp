c10f71c2efb898048a2cacc4aa918a2351ba8679
Kalyan Pothula <96769120+Pothulakalyan@users.noreply.github.com>
2025-06-27 11:27:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k largest elements/k-largest-elements.cpp
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -2,27 +2,22 @@ class Solution {
   public:
     vector<int> kLargest(vector<int>& arr, int k) {
         // Your code here
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-
-        for(int i = 0; i < arr.size(); i++) {
-            if (minHeap.size() < k) {
-                minHeap.push(arr[i]);
-            } else if (arr[i] > minHeap.top()) {
-                minHeap.pop();
-                minHeap.push(arr[i]);
-            }
-        }
-
-        // Extract elements into result vector
-        vector<int> result;
-        while(!minHeap.empty()) {
-            result.push_back(minHeap.top());
-            minHeap.pop();
-        }
-
-        // Sort in descending order
-        sort(result.begin(), result.end(), greater<int>());
-
-        return result;
+       priority_queue<int,vector<int>,greater<int>>minHeap;
+       for(int i=0;i<arr.size();i++){
+           if(minHeap.size()<k){
+               minHeap.push(arr[i]);
+           }
+           else if(arr[i]>minHeap.top()){
+               minHeap.push(arr[i]);
+               minHeap.pop();
+           }
+       }
+       vector<int>ans;
+       while(!minHeap.empty()){
+           ans.push_back(minHeap.top());
+           minHeap.pop();
+       }
+       sort(ans.begin(),ans.end(),greater<int>());
+       return ans;
     }
 };
